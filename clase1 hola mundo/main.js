ec11ff8f6adf290ea89c8c99c5b7a3175bfdc264
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-08-17 17:51:48 -0500

    hacemos los primeros ejercicios con vue.js

---
+++ clase1 hola mundo/main.js
@@ -0,0 +1,12 @@
+/* creamos una constante para entrar a vue
+el nombre app puede ser cualquier nombre */
+const app = Vue.createApp({
+    /* la data es donde nosotros vamos a ver reflejados los datos en la vista */
+    data() {
+        return {
+            titulo:'este es un hola mundo con vue js',
+            cantidad: 1000,
+            enlace: 'http://ticdwem.com'
+        }
+    },
+})
