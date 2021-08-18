41650d4c0278721f4fde9a4986623cb69970262d
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-08-18 18:00:33 -0500

    agregamos un menu

--- clase1 hola mundo/components/MenuHeader.js
+++ clase1 hola mundo/components/MenuHeader.js
@@ -1,13 +1,13 @@
 app.component('menu-header', {
     template: /*html*/`
-                        <nav class="ml-5">
+                        <nav>
                             <ul>
                                 <li v-for="(item, index) in links" :key="index"><a  v-bind:href="index">{{item}}</a></li>
                             </ul>
                         </nav>`,
     data() {
         return {
-            links: {'index.html':'hola mundo', 'renderizar.html':'renderizar', 'v-bind.html':'atributo v-bind', 'ifelse.html':'v-if v-else', 'vfor.html':'v-fo', 'eventoClick.html':'Agregar con boton', 'clasdinamico.html':'Class Dinamico', 'componentes.html':'Componenetes'}
+            links: {'index.html':'hola mundo', 'renderizar.html':'renderizar', 'v-bind.html':'atributo v-bind', 'ifelse.html':'v-if v-else', 'vfor.html':'v-fo', 'eventoClick.html':'Agregar con boton', 'clasdinamico.html':'Class Dinamico', 'componentes.html':'Componenetes','comunicacionComponentes.html':'comunicaion entre componentes'}
         }
 
     }
