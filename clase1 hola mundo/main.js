89f359945fe65c02c0853e13129be72cbb97dd26
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-08-17 23:08:30 -0500

    sumamos texto
    aprendimos a usar un class dinamico, a usar el evento click, condicionales v-if v-else-if y v-else, a usar un render que practiccamente es lo mismo que if else, v-bind que sirve para injectar codigo de enlaces y v for que es pararecorrer array que tenemos en la data

--- clase1 hola mundo/main.js
+++ clase1 hola mundo/main.js
@@ -7,8 +7,33 @@ const app = Vue.createApp({
             titulo:'este es un hola mundo con vue js',
             cantidad: 1000,
             enlace: 'http://ticdwem.com',
-            estado:false,
-            servicios:['tranferencias','pagos','cheques']
+            estado:true,
+            servicios:['tranferencias','pagos','cheques'],
+            desactivar:false /* creasmos esta propiedad para decir que cuando esta en false quite el disbled del html */
         }
     },
+    methods: {
+        agergarSaldo(){            
+            this.cantidad = this.cantidad + 100
+            this.desactivar = false
+            this.estado=true
+        },
+        disminuirSaldo(){
+            if(this.cantidad === 0){
+                this.desactivar = true
+                this.estado = false
+                alert ("ya no tienes saldo")
+                return
+            }
+                this.cantidad = this.cantidad - 100
+
+           
+        }
+    },
+    computed:{
+        /* LAS CLASES COMPUTADAS SIEMPRE DEBEN REORNAR ALGO */
+        colorCantidad(){
+            return this.cantidad > 600 ? 'text-success' : 'text-danger'
+        }
+    }
 })
